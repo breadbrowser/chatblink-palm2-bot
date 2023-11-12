b7c2bd73409604fa8580556f2c2423299b8b8fb7
breadbrowser <64813014+breadbrowser@users.noreply.github.com>
2023-11-12 13:03:11 -0500

    Update console.js

--- console.js
+++ console.js
@@ -1,6 +1,32 @@
 oldmessage=''
 newMessage=''
-function aibot() {
+
+function getms() {
+let messages = document.querySelectorAll('#msgs .stranger');
+let lastMessage = messages[messages.length - 1].innerText;
+console.log(lastMessage);
+newMessage=lastMessage
+}
+
+function aibot(input) {
+let myVariable;
+
+fetch('http://localhost:625/generate', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({message: input})
+})
+.then(res => res.json())
+.then(data => {
+  console.log(data);
+  myVariable = data;
+});
+}
+
+//find stranger
+function sag() {
 if(stranger_id==false) {
   console.log("finding stranger");
   document.getElementById("next-stranger")[0].click();
@@ -17,5 +43,18 @@ if(stranger_id==false) {
   console.log("stranger is connected");
   $("#msg").val('hi');
   document.getElementById('send_message').click();
-};
+}
 
+
+
+
+
+
+
+
+
+
+
+
+
+};
